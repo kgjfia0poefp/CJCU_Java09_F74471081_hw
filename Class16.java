4610b739f652541a1276c0b981c08e8b4be42140
kgjfia0poefp <92013723+kgjfia0poefp@users.noreply.github.com>
2021-11-06 14:19:21 +0800

    Add files via upload

---
+++ Class16.java
@@ -0,0 +1,31 @@
+package Java09;
+class CCircle
+{
+	private static double pi=3.14;
+	private double radius;
+
+	public CCircle(double r)
+	{
+		radius=r;
+	}
+	public void show()
+	{
+		System.out.print("radius="+radius);
+		System.out.println(", area="+pi*radius*radius);
+	}
+}
+public class Class16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CCircle circle[];
+		circle=new CCircle[6];
+		
+		for(int i=0;i<circle.length;i++)
+		{
+			circle[i]=new CCircle((double)i);
+			circle[i].show();
+		}
+	}
+
+}
