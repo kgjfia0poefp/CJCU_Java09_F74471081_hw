f7a1b4cfbec22af91e39c5602055c9ff4304d0c4
kgjfia0poefp <92013723+kgjfia0poefp@users.noreply.github.com>
2021-11-06 13:42:53 +0800

    Add files via upload

---
+++ Class05.java
@@ -0,0 +1,44 @@
+package Java09;
+class CCount
+{
+	private static int cnt=0;
+
+	public CCount()
+	{
+		count();
+	}
+	public static void count()
+	{
+		cnt++;
+	}
+	public static void setZero()
+	{
+		cnt=0;
+	}
+	public static void setValue(int n)
+	{
+		cnt=n;
+	}
+	public static void show()
+	{
+		System.out.println("cnt="+cnt);
+	}
+}
+public class Class05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CCount cn1=new CCount();
+		CCount cn2=new CCount();
+		CCount.show();
+		
+		CCount.setZero();
+		System.out.println("using setZero()...");
+		CCount.show();
+		
+		CCount.setValue(10);
+		System.out.println("using setValue(10)...");
+		CCount.show();
+	}
+
+}
