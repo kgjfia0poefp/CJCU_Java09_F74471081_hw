7f6443c487eac7899315133869b26e60da82b4ab
kgjfia0poefp <92013723+kgjfia0poefp@users.noreply.github.com>
2021-11-06 14:43:29 +0800

    Add files via upload

---
+++ Class19.java
@@ -0,0 +1,44 @@
+package Java09;
+class Data
+{
+	private String name;
+	private Test score;
+
+	class Test
+	{
+		private int english;
+		private int math;
+
+		public Test(int eng,int m)
+		{
+			english=eng;
+			math=m;
+		}
+		public double avg()
+		{
+			return (english+math)/2.0;
+		}
+	}
+	public Data(String na,int eng,int ma)
+	{
+		name=na;
+		score=new Test(eng,ma);
+	}
+	public void show()
+	{
+	System.out.println("學生姓名:"+name);
+	System.out.println("英文成績:"+score.english);
+	System.out.println("數學成績:"+score.math);
+	System.out.println("平均成績:"+score.avg());
+	}
+	
+}
+public class Class19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Data stu=new Data("Annie",85,92);
+		stu.show();
+	}
+
+}
