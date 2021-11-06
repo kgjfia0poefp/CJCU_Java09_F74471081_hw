d36fac013c83bdbceee94b9a7345d838142dd814
kgjfia0poefp <92013723+kgjfia0poefp@users.noreply.github.com>
2021-11-06 13:33:11 +0800

    Add files via upload

---
+++ Class01_c.java
@@ -0,0 +1,34 @@
+package Java09;
+class CRectangle
+{
+	int width;
+	int height;
+
+	public CRectangle(int w,int h)
+	{
+		width=w;
+		height=h;
+		System.out.println("constructor CRectangle(int w,int h) called");
+	}
+	public CRectangle()
+	{
+		this(10,8);
+		System.out.println("constructor CRectangle() called");
+	}
+	public void show()
+	{
+		System.out.println("width="+width);
+		System.out.println("height="+height);
+	}
+}
+public class Class01_c {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CRectangle rec1=new CRectangle(5,2);
+		rec1.show();
+		CRectangle rec2=new CRectangle();
+		rec2.show();
+	}
+
+}
